69cf9978fa61c28c0f877af1af65d6afbfd20f93
Hikari Hayashi <rev.hikari@gmail.com>
2022-11-08 18:08:00 +0800

    Reorganize config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   reportUnusedDisableDirectives: true,
 
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+
+  env: {
+    es2022: true,
+  },
+
+  plugins: ["simple-import-sort"],
+  extends: ["eslint:recommended"],
+
+  rules: {
+    eqeqeq: "error",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+  },
+
   overrides: [
     {
       files: ["**/*.{ts,tsx,mts,cts}"],
@@ -12,41 +29,16 @@ module.exports = {
         project: ["**/tsconfig.json"],
       },
 
-      plugins: ["@typescript-eslint", "simple-import-sort"],
+      plugins: ["@typescript-eslint"],
       extends: [
-        "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
 
       rules: {
-        eqeqeq: "error",
         "@typescript-eslint/no-duplicate-imports": "error",
         "@typescript-eslint/consistent-type-imports": "error",
         "@typescript-eslint/consistent-type-exports": "error",
-        "simple-import-sort/imports": "warn",
-        "simple-import-sort/exports": "warn",
-      },
-    },
-
-    {
-      files: ["**/*.{js,mjs,cjs}"],
-
-      parserOptions: {
-        ecmaVersion: "latest",
-      },
-
-      env: {
-        es2022: true,
-      },
-
-      plugins: ["simple-import-sort"],
-      extends: ["eslint:recommended"],
-
-      rules: {
-        eqeqeq: "error",
-        "simple-import-sort/imports": "warn",
-        "simple-import-sort/exports": "warn",
       },
     },
   ],
