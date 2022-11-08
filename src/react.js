ae90a0be3e17620c34af2fcdd2cd0217b15cdaa8
Hikari Hayashi <rev.hikari@gmail.com>
2022-11-08 18:01:57 +0800

    Update react rules

--- src/react.js
+++ src/react.js
@@ -3,17 +3,11 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   extends: require.resolve("."),
 
-  overrides: [
-    {
-      files: ["**/*.{ts,tsx,mts,cts}"],
+  plugins: ["react", "react-hooks"],
+  extends: ["plugin:react-hooks/recommended"],
 
-      plugins: ["react", "react-hooks"],
-      extends: ["plugin:react-hooks/recommended"],
-
-      rules: {
-        "react/no-deprecated": "error",
-        "react/jsx-key": "error",
-      },
-    },
-  ],
+  rules: {
+    "react/no-deprecated": "error",
+    "react/jsx-key": "error",
+  },
 };
