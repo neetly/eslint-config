f6f92afc8fc14e74abb91b2eed6e734817523a43
Hikari Hayashi <rev.hikari@gmail.com>
2022-11-10 19:09:27 +0800

    Fix config file

--- src/react.js
+++ src/react.js
@@ -1,10 +1,8 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  extends: require.resolve("."),
-
   plugins: ["react", "react-hooks"],
-  extends: ["plugin:react-hooks/recommended"],
+  extends: [require.resolve("."), "plugin:react-hooks/recommended"],
 
   rules: {
     "react/no-deprecated": "error",
