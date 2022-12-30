22347d7f805f8da75f944636a4621c6d18715f49
Hikari Hayashi <rev.hikari@gmail.com>
2022-12-31 02:16:04 +0800

    Enable jsx-a11y recommended rule set

--- src/react.js
+++ src/react.js
@@ -1,8 +1,12 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  plugins: ["react", "react-hooks"],
-  extends: [require.resolve("."), "plugin:react-hooks/recommended"],
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  extends: [
+    require.resolve("."),
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
 
   rules: {
     "react/no-deprecated": "error",
